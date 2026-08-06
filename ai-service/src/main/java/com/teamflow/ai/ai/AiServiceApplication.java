128bab6f8e1df8cc300d875876448c1010e4e5d2
PurveshhpatilGit <purveshpatil1610@gmail.com>
2026-08-06 17:02:38 +0530

    microservices part

---
+++ ai-service/src/main/java/com/teamflow/ai/ai/AiServiceApplication.java
@@ -0,0 +1,14 @@
+package com.teamflow.ai.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class AiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiServiceApplication.class, args);
+    }
+}
---
+++ project-service/src/main/java/com/teamflow/ai/project/ProjectServiceApplication.java
@@ -0,0 +1,14 @@
+package com.teamflow.ai.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class ProjectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectServiceApplication.class, args);
+    }
+}
